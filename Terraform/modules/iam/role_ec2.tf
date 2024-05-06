0370c8ad0a0998e2886095b94dc4367f253d90d6
Michael Schams <noreply@example.com>
2024-05-06 16:10:42 +1000

    Configure CloudWatch Agent IAM permissions to write CloudWatch metrics

--- Terraform/modules/iam/role_ec2.tf
+++ Terraform/modules/iam/role_ec2.tf
@@ -5,3 +5,21 @@ resource "aws_iam_role" "ec2" {
     assume_role_policy = file("${path.module}/json/assume_role_ec2.json")
     tags = var.tags
 }
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent_admin_policy" {
+    policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentAdminPolicy"
+    role = aws_iam_role.ec2.name
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent_server_policy" {
+    policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+    role = aws_iam_role.ec2.name
+}
+
+# Inline policy
+# Allowing the CloudWatch agent to set log retention policy
+resource "aws_iam_role_policy" "set_log_retention_policy" {
+    name = "set-log-retention-policy"
+    role = aws_iam_role.ec2.id
+    policy = file("${path.module}/json/set_log_retention_policy.json")
+}
